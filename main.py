498d360f6e011588fbe6359713173d5c5fa91699
Shubham Bhatnagar <shubham.bhatnagar@pm.me>
2025-12-02 12:43:27 -0500

    hello world commit?

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from zed-flow-extenstion!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,24 @@
+import sys
+import webbrowser  # Not external
+from pathlib import Path
+
+from flowlauncher import FlowLauncher  # external library
+
+plugindir = Path.absolute(Path(__file__).parent)
+paths = (".", "lib", "plugin")
+sys.path = [str(plugindir / p) for p in paths] + sys.path
+
+class HelloWorld(FlowLauncher):
+    def query(self, query):
+        return [
+            {
+                "Title": "Hello World, this is where title goes. {}".format(('Your query is: ' + query , query)[query == '']),
+                "SubTitle": "This is where your subtitle goes, press enter to open Flow's url",
+                "IcoPath": "Images/app.png",
+                "ContextData": ["foo", "bar"]
+                "JsonRPCAction": {
+                    "method": "open_url",
+                    "parameters": ["https://github.com/Flow-Launcher/Flow.Launcher"]
+                }
+            }
+        ]
